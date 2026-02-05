ca7f330e422abbea5185124ecd0ff13238517f55
keroro <keroro714@qq.com>
2026-02-05 17:29:59 +0800

    feat: 238-除了自身以外数组的乘积

--- src/main/java/xyz/keroro/Main.java
+++ src/main/java/xyz/keroro/Main.java
@@ -4,6 +4,6 @@ import xyz.keroro.question.Solution;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.reverseWords("a good   example"));
+        System.out.println(Solution.productExceptSelf(new int[]{1, 2, 3, 4}));
     }
 }
